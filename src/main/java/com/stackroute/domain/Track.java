e86e2b0ccba072a9d492c52ef9be892af463d920
gekadal1 <gekadal1@in.ibm.com>
2019-01-17 15:39:32 +0530

    Muzix

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -6,54 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Track {
     @Id
     int trackId;
     String trackName;
     String comments;
 
-    public Track() {
-
-    }
-
-    public Track(int trackId, String trackName, String comments) {
-        this.trackId = trackId;
-        this.trackName = trackName;
-        this.comments = comments;
-    }
-
-    @Override
-    public String toString() {
-        return "Track{" +
-                "trackId=" + trackId +
-                ", trackName='" + trackName + '\'' +
-                ", comments='" + comments + '\'' +
-                '}';
-    }
-
-    public int getTrackId() {
-        return trackId;
-    }
-
-    public void setTrackId(int trackId) {
-        this.trackId = trackId;
-    }
-
-    public String getTrackName() {
-        return trackName;
-    }
-
-    public void setTrackName(String trackName) {
-        this.trackName = trackName;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
 }
 
